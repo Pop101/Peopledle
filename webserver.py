1c3c8dd367716a875860b58c25c4c55ad534279a
Leon Leibmann <leon.leibmann@gmail.com>
2022-12-18 01:45:25 -0800

    Finish pick_person, add retrieval endpoint

--- webserver.py
+++ webserver.py
@@ -7,9 +7,13 @@ from random import choice
 import requests
 from re import sub, search, finditer, IGNORECASE
 from time import time
+from modules.mongrel_db import MongrelDB
+from anyascii import anyascii
+
+MAX_GUESSES = 5
 
 app = Flask(__name__)
-
+db = MongrelDB("./data")
 data = {"response": "OK", "guess": 2, "result": {"solved": False, "next_hint": "This is a hint"}}
 
 
@@ -24,12 +28,46 @@ def post():
     return data
 
 
-def pick_person():
-    global language
-    with open("languages.json") as f:
-        langs = load(f)
+@app.route("/names", methods=["GET"])
+def get_valid_people() -> list[str]:
+    people = list()
+    for person in db:
+        people.append(anyascii(db[person]["name"]))
+    return people
 
-    language = choice(langs)
+
+def pick_person():
+    global person
+
+    def single_pick():
+        full_data = db[choice(list(db))]
+
+        person = {
+            "name": full_data["name"],
+            "summary": full_data["summary"],
+            "hints": list(filter(lambda x: bool(x), full_data["category"])),
+            "guesses": list(),
+        }
+
+        # Split sentences list into equal parts
+        # Choose a sentence from each part
+        summary = person["summary"]
+        summary = list(filter(lambda x: len(x) < 400, summary))
+        if len(summary) < MAX_GUESSES:
+            return False
+
+        chunk_size = len(person["summary"]) // MAX_GUESSES
+        for i in range(MAX_GUESSES):
+            person["guesses"].insert(0, choice(summary[i * chunk_size : (i + 1) * chunk_size]))
+
+        return person
+
+    i, person = 0, None
+    while i < 100 and not person:
+        person = single_pick()
+        i += 1
+
+    return person
 
 
 if __name__ == "__main__":
