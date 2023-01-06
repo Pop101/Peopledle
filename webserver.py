e682fa8c6d5b9375ff6aa0056353d51f2f2ade2c
Leon Leibmann <leon.leibmann@gmail.com>
2023-01-06 00:28:28 -0800

    Rewrite person selection to be Deterministic

--- webserver.py
+++ webserver.py
@@ -4,23 +4,25 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from random import choice
 from modules.mongrel_db import MongrelDB
 from anyascii import anyascii
+import hashlib
+
+hash = lambda x: int(hashlib.sha256(repr(x).encode("utf-8")).hexdigest(), 16) # Deterministic hash
 
 MAX_GUESSES = 5
-
 app = Flask(__name__)
 db = MongrelDB("./data")
-data = {"response": "OK", "guess": 2, "result": {"solved": False, "next_hint": "This is a hint"}}
-person = dict()
-
+current_day = 1
 
 @app.route("/", methods=["GET"])
 def index():
+    person = get_person(current_day)
     return render_template("index.html", info=person)
 
 
 @app.route("/", methods=["POST"])
 def post():
     print("GOT GUESS", request.json)
+    person = get_person(current_day)
     return {"response": "OK", "result": {"next_hint": person["guesses"][request.json["guesses"]]}}
 
 
@@ -36,68 +38,36 @@ def get_valid_people() -> list[str]:
     return people
 
 
-def pick_person():
-    """Pick a random person from the database,
-    populating the global variable `person` with their data.
+def increment_person():
+    global current_day
+    
+    current_day += 1
+    
+    # Save to file
+    with open("day.txt", "w") as f:
+        f.write(str(current_day))
 
-    This function is called on startup and every day at local midnight.
-
-    Returns:
-        _type_: _description_
-    """
-    global person
-
-    # Subfunction to pick a single person
-    # Returns JSON object with that person's data
-    #    or False if that person was invalid
-    def single_pick():
-        # Testing code to hard-code a choice
-        full_data = db['A C Bhaktivedanta Swami Prabhupada']
-
-        # full_data = db[choice(list(db))]
-
-        person = {
-            "name": full_data["name"],
-            "summary": full_data["summary"],
-            "hints": list(filter(lambda x: bool(x), full_data["categories"])),
-            "guesses": list(),
-        }
-
-        # Split sentences list into equal parts
-        # Choose a sentence from each part
-        summary = full_data["sentences"]
-        summary = list(filter(lambda x: len(x) < 400, summary))
-        if len(summary) < MAX_GUESSES:
-            return False
-
-        chunk_size = len(summary) // MAX_GUESSES
-        for i in range(MAX_GUESSES):
-            person["guesses"].insert(0, choice(summary[i * chunk_size : (i + 1) * chunk_size]))
-
-        return person
-
-    i, person = 0, None
-    while i < 100 and not person:
-        person = single_pick()
-        i += 1
-
-    return person
-
+def get_person(day:int):
+    person_name = list(db)[hash(day) % len(db)]
+    return db[person_name]
 
 if __name__ == "__main__":
-    pick_person()
+    # Try to load current day from file
+    # subtract 1 as we increment it later
+    # if no file is found, we start at 0
+    try:
+        with open("day.txt", "r") as f:
+            current_day = int(f.read()) - 1
+    except FileNotFoundError:
+        pass
+    increment_person()
 
     apsched = BackgroundScheduler()
     apsched.start()
-    apsched.add_job(pick_person, "cron", day="*", hour="0")
-
-    # Quick endpoint integrity test
-    with app.test_client() as c:
-        rv = c.post("/submit", json=data)
-        pass
+    apsched.add_job(increment_person, "cron", day="*", hour="0")
 
     # Start the server
     print("Starting server on port 8787")
-    print(person)
+    print(get_person(current_day)["name"])
     # USE WAITRESS IN PRODUCTION: serve(app, host="0.0.0.0", port=8787)
     app.run(debug=True)
